b242702d22c5368f6cbca90268f9248debddf451
Maks Tkachenko <tkachenkonino1996@gmail.com>
2023-09-22 16:28:48 +0300

    feat(*): add chalk

--- src/cli/commands/help.command.ts
+++ src/cli/commands/help.command.ts
@@ -1,5 +1,5 @@
 import { Command } from './command.interface.js';
-
+import chalk from 'chalk';
 export class HelpCommand implements Command {
   public getName(): string {
     return '--help';
@@ -7,14 +7,14 @@ export class HelpCommand implements Command {
 
   public async execute(): Promise<void> {
     console.info(`
-    Программа для подготовки данных для REST API сервера.
+    ${chalk.bgYellow('Программа для подготовки данных для REST API сервера.')}
     Пример:
-        cli.js --<command> [--arguments]
+      cli.js ${chalk.cyan('--<command>')} [--arguments]
     Команды:
-        --version:                   # выводит номер версии
-        --help:                      # печатает этот текст
-        --import <path>:             # импортирует данные из TSV
-        --generate <n> <path> <url>  # генерирует произвольное количество тестовых данных
+      ${chalk.cyan('--version')}:                   ${chalk.magenta('# выводит номер версии')}
+      ${chalk.cyan('--help')}:                      ${chalk.magenta('# печатает этот текст')}
+      ${chalk.cyan('--import')} <path>:             ${chalk.magenta('# импортирует данные из TSV')}
+      ${chalk.cyan('--generate')} <n> <path> <url>  ${chalk.magenta('# генерирует произвольное количество тестовых данных')}
   `);
   }
 }
