970976591f7defee6504a968949eeab021e65098
Maks Tkachenko <tkachenkonino1996@gmail.com>
2023-09-30 21:31:15 +0300

    feat(*): add convict valid on init env

--- src/shared/libs/config/rest.config.ts
+++ src/shared/libs/config/rest.config.ts
@@ -16,7 +16,7 @@ export class RestConfig implements Config<RestSchema> {
       throw new Error('Can\'t read .env file. Perhaps the file does not exists.');
     }
     configRestSchema.load({});
-    // configRestSchema.validate({ allowed: 'strict', output: this.logger.info });
+    configRestSchema.validate({ allowed: 'strict', output: this.logger.info });
     this.config = configRestSchema.getProperties();
     this.logger.info('.env file found and successfully parsed!');
   }
