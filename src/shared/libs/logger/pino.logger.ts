5121858d3824fe37cd313ea92e2b173b040fe837
Maks Tkachenko <tkachenkonino1996@gmail.com>
2023-09-30 15:02:50 +0300

    feat(*): add write debug into file

--- src/shared/libs/logger/pino.logger.ts
+++ src/shared/libs/logger/pino.logger.ts
@@ -1,10 +1,31 @@
+import { resolve } from 'node:path';
+import { getCurrentModuleDirectoryPath } from '../../helpers/index.js';
 import { Logger } from './logger.interface.js';
-import {Logger as PinoInstance, pino} from 'pino';
+import {Logger as PinoInstance, pino, transport} from 'pino';
 export class PinoLogger implements Logger {
   public readonly logger: PinoInstance;
 
   constructor() {
-    this.logger = pino();
+    const modulePath = getCurrentModuleDirectoryPath();
+    const logFilePath = 'logs/rest.log';
+    const destination = resolve(modulePath, '../../../', logFilePath);
+
+    const multiTransport = transport({
+      targets: [
+        {
+          target: 'pino/file',
+          options: { destination },
+          level: 'debug'
+        },
+        {
+          target: 'pino/file',
+          level: 'info',
+          options: {},
+        }
+      ],
+    });
+
+    this.logger = pino(multiTransport);
   }
 
   info(message: string, ...args: unknown[]): void {
