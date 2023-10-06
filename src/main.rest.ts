ba4d683e1de28924289c751043041c73d11d7c04
Maks Tkachenko <tkachenkonino1996@gmail.com>
2023-10-06 22:52:43 +0300

    refactor(*): add async init restApp

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -6,10 +6,10 @@ import { createRestApplication } from './rest/rest.container.js';
 import { createUserContainer } from './shared/modules/user/index.js';
 import { createOfferContainer } from './shared/modules/offer/offer.container.js';
 
-const bootstrap = () => {
+const bootstrap = async () => {
   const appContainer = Container.merge(createRestApplication(), createUserContainer(), createOfferContainer());
   const restApplication = appContainer.get<RestApplication>(Component.RestApplication);
-  restApplication.init();
+  await restApplication.init();
 };
 
 bootstrap();
